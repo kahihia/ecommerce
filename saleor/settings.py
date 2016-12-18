b66bed1c77f847a1718a806a97ac2ee7bfc28f84
Luigi Gianni <lcgianni@gmail.com>
2016-12-18 15:45:59 -0200

    pagseguro out of sandbox

--- saleor/settings.py
+++ saleor/settings.py
@@ -308,8 +308,8 @@ WEBPACK_LOADER = {
             r'.+\.hot-update\.js',
             r'.+\.map']}}
 
-PAGSEGURO_EMAIL = 'lcgianni@gmail.com'
-PAGSEGURO_TOKEN = '2D4C32F4A0F74AF18276FB65086F3BC2'
-PAGSEGURO_SANDBOX = True # se o valor for True, as requisicoes a api serao feitas usando o PagSeguro Sandbox.
+PAGSEGURO_EMAIL = os.environ.get('PAGSEGURO_EMAIL')
+PAGSEGURO_TOKEN = os.environ.get('PAGSEGURO_TOKEN')
+PAGSEGURO_SANDBOX = os.environ.get('PAGSEGURO_SANDBOX') # se o valor for True, as requisicoes a api serao feitas usando o PagSeguro Sandbox.
 PAGSEGURO_LOG_IN_MODEL = True # se o valor for True, os checkouts e transacoes vao ser logadas no database.
 SECURE_REDIRECT_EXEMPT = 'retorno/pagseguro/'
