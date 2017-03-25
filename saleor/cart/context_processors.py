69d0ce26af49eaed193322b0291c63129339fab8
bcgianni <bcgianni@gmail.com>
2017-03-24 22:42:26 -0300

    return total to view

--- saleor/cart/context_processors.py
+++ saleor/cart/context_processors.py
@@ -5,4 +5,6 @@ from .decorators import get_cart_from_request
 def cart_counter(request):
     """ Return number of items from cart """
     cart = get_cart_from_request(request)
-    return {'cart_counter': cart.quantity}
+    cart_total = cart.get_total().gross
+    cart_quantity = cart.quantity
+    return {'cart_counter': cart_quantity, 'cart_total': cart_total}
